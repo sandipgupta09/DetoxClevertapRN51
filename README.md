4fa7b494067c8ef6f15fa6fdfe4bb1a805b00eb3
Sandip Gupta <sandipguptadhn@gmail.com>
2018-08-17 16:52:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DetoxClevertapRN51
---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,11 @@
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+
+  
+})
